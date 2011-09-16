ealimonda/magickeyboard

commit d2142ee28b4188399f0aabdafdb4d24bd1f7123d

Added devices list to the Preferences window

Sources/MKController.m
@@ -113,31 +113,20 @@ CFMutableArrayRef MTDeviceCreateList(void); //returns a CFMutableArrayRef array 
 	NSMutableArray *deviceList = (NSMutableArray *)MTDeviceCreateList(); //grab our device list
 	for (NSUInteger i = 0; i < [deviceList count]; i++) {
 		MKDevice *thisDevice = [MKDevice deviceWithMTDeviceRef:(MTDeviceInfo *)[deviceList objectAtIndex:i] ID:i];
+		if (!thisDevice)
+			continue;
 #ifdef __DEBUGGING__
 		NSLog(@"Checking device: %@", [thisDevice getInfo]);
 #endif // __DEBUGGING__
+		NSLog(@"Detected %@ (#%lu).", [thisDevice deviceType], i);
+		if ([thisDevice  isUsable])
+			[thisDevice setEnabled:YES];
+		[[self devices] addObject:thisDevice];
 		if (![thisDevice isValid]) {
 			NSLog(@"Unrecognized device (#%lu), please report.\nDevice info: %@", i, [thisDevice getInfo]);
-			if (!thisDevice)
-				continue;
 		}
-		switch ([thisDevice family]) {
-		case kDeviceFamilyMBPTrackpad:
-			NSLog(@"Detected MacBook Pro trackpad (#%lu).", i);
-			break;
-		case kDeviceFamilyMagicMouse:
-			NSLog(@"Detected Magic Mouse (#%lu).  Ignoring it.", i);
+		if (![thisDevice isUsable])
 			continue;
-		case kDeviceFamilyMagicTrackpad:
-			NSLog(@"Detected Magic Trackpad (#%lu).", i);
-			break;
-		default:
-			NSLog(@"Detected device (#%lu) family %d.  Ignoring it.", i, [thisDevice family]);
-			NSLog(@"Device info: %@", [thisDevice getInfo]);
-			continue;
-		}
-		[thisDevice setEnabled:YES];
-		[[self devices] addObject:thisDevice];
 		MTRegisterContactFrameCallback([deviceList objectAtIndex:i], callback); //assign callback for device
 		MTDeviceStart([deviceList objectAtIndex:i], 0); //start sending events
 	}

Resources/English.lproj/MainMenu.xib
@@ -4115,14 +4115,12 @@
 						<bool key="EncodedWithXMLCoder">YES</bool>
 						<object class="NSArray" key="dict.sortedKeys">
 							<bool key="EncodedWithXMLCoder">YES</bool>
-							<string>checkForUpdates:</string>
 							<string>disableTrackingSelector:</string>
 							<string>quitSelector:</string>
 							<string>submitFeedback:</string>
 						</object>
 						<object class="NSMutableArray" key="dict.values">
 							<bool key="EncodedWithXMLCoder">YES</bool>
-							<string>id</string>
 							<string>id</string>
 							<string>id</string>
 							<string>id</string>
@@ -4132,17 +4130,12 @@
 						<bool key="EncodedWithXMLCoder">YES</bool>
 						<object class="NSArray" key="dict.sortedKeys">
 							<bool key="EncodedWithXMLCoder">YES</bool>
-							<string>checkForUpdates:</string>
 							<string>disableTrackingSelector:</string>
 							<string>quitSelector:</string>
 							<string>submitFeedback:</string>
 						</object>
 						<object class="NSMutableArray" key="dict.values">
 							<bool key="EncodedWithXMLCoder">YES</bool>
-							<object class="IBActionInfo">
-								<string key="name">checkForUpdates:</string>
-								<string key="candidateClassName">id</string>
-							</object>
 							<object class="IBActionInfo">
 								<string key="name">disableTrackingSelector:</string>
 								<string key="candidateClassName">id</string>

Sources/MKDevice.h
@@ -89,7 +89,10 @@ typedef struct MTDeviceInfo MTDeviceInfo;
 
 - (NSString *)getInfo;
 
+- (NSString *)deviceType;
+
 - (BOOL)isValid;
+- (BOOL)isUsable;
 
 //- (uint32)unk_v0;
 - (uint32)unk_k0;
@@ -97,7 +100,7 @@ typedef struct MTDeviceInfo MTDeviceInfo;
 - (uint32)unk_k1;
 //- (uint32)unk_v2;
 - (uint32)unk_k2;
-//- (uint32)address;
+- (uint32)address;
 //- (uint32)unk_v3;
 - (uint32)family;
 //- (uint32)bcdver;

Sources/MKDevice.m
@@ -137,6 +137,19 @@ const MTDeviceInfo multiTouchSampleDevice = {
 	];
 }
 
+- (NSString *)deviceType {
+	switch ([self family]) {
+	case kDeviceFamilyMBPTrackpad:
+		return @"MacBook Pro Trackpad";
+	case kDeviceFamilyMagicTrackpad:
+		return @"Magic Trackpad";
+	case kDeviceFamilyMagicMouse:
+		return @"Magic Mouse";
+	default:
+		return [NSString stringWithFormat:@"Unknown Device (family 0x%x)", [self family]];
+	}
+}
+
 #pragma mark Special accessors
 - (BOOL)isValid {
 	MKDevice *sampleDevice = [MKDevice sampleDevice];
@@ -150,6 +163,19 @@ const MTDeviceInfo multiTouchSampleDevice = {
 		return NO;
 	return YES;
 
+}
+
+- (BOOL)isUsable {
+	if (![self isValid])
+		return NO;
+	switch ([self family]) {
+	case kDeviceFamilyMBPTrackpad:
+	case kDeviceFamilyMagicTrackpad:
+		return YES;
+	case kDeviceFamilyMagicMouse:
+	default:
+		return NO;
+	}
 }
 
 //- (uint32)unk_v0;
@@ -167,7 +193,9 @@ const MTDeviceInfo multiTouchSampleDevice = {
 	return device.unk_k2;
 }
 
-//- (uint32)address;
+- (uint32)address {
+	return device.address;
+}
 //- (uint32)unk_v3;
 - (uint32)family {
 	return device.family;

Sources/MKPreferencesController.h
@@ -21,6 +21,12 @@
 	IBOutlet NSTableColumn *layoutsTableSymbol;
 	IBOutlet NSTableColumn *layoutsTableEnabled;
 	IBOutlet NSTableColumn *layoutsTableLayoutName;
+	
+	IBOutlet NSTableView *devicesTableView;
+	IBOutlet NSTableColumn *devicesTableCurrent;
+	IBOutlet NSTableColumn *devicesTableEnabled;
+	IBOutlet NSTableColumn *devicesTableType;
+	IBOutlet NSTableColumn *devicesTableID;
 }
 
 @end

Sources/MKPreferencesController.m
@@ -17,6 +17,7 @@
 #import "MagicKeyboardAppDelegate.h"
 #import "MKController.h"
 #import "MKLayout.h"
+#import "MKDevice.h"
 
 #pragma mark Implementation
 @implementation MKPreferencesController
@@ -52,6 +53,17 @@
 		if (aTableColumn == layoutsTableEnabled)
 			return [NSNumber numberWithBool:[[[sharedController currentLayout] layoutIdentifier]
 							 isEqualToString:[currentItem layoutIdentifier]]];
+	} else if (aTableView == devicesTableView) {
+		NSArray *devices = [sharedController devices];
+		MKDevice *currentItem = [devices objectAtIndex:rowIndex];
+		if (aTableColumn == devicesTableCurrent)
+			return [NSNumber numberWithBool:[currentItem isEnabled]];
+		if (aTableColumn == devicesTableEnabled)
+			return [NSNumber numberWithBool:[currentItem isUsable]];
+		if (aTableColumn == devicesTableType)
+			return [currentItem deviceType];
+		if (aTableColumn == devicesTableID)
+			return [NSString stringWithFormat:@"0x%X", [currentItem address]];
 	}
 	// Unknown table!
 	return nil;
@@ -61,6 +73,8 @@
 	      row:(NSInteger)rowIndex {
 #pragma unused (anObject, aTableColumn, rowIndex)
 	if (aTableView == layoutsTableView) {
+		return;
+	} else if (aTableView == devicesTableView) {
 		return;
 	}
 	// Unknown table!
@@ -72,6 +86,8 @@
 	MKController *sharedController = [sharedAppDelegate controller];
 	if (aTableView == layoutsTableView) {
 		return [[[sharedController layouts] allValues] count];
+	} else if (aTableView == devicesTableView) {
+		return [[sharedController devices] count];
 	}
 	// Unknown table!
 	return 0;

commit cc8bbe58d748e30d2e22fc6bdf400a2b7b0305c3

Fixed startup message when no compatible devices are found.
Was broken since d2142ee28b41 (preferences devices list)

Sources/MKController.m
@@ -109,6 +109,7 @@ CFMutableArrayRef MTDeviceCreateList(void); //returns a CFMutableArrayRef array 
 	[self switchToLayoutNamed:layout];
 
 	NSMutableArray *deviceList = (NSMutableArray *)MTDeviceCreateList(); //grab our device list
+	BOOL foundUsableDevice = NO;
 	for (NSUInteger i = 0; i < [deviceList count]; i++) {
 		MKDevice *thisDevice = [MKDevice deviceWithMTDeviceRef:(MTDeviceInfo *)[deviceList objectAtIndex:i] ID:i];
 		if (!thisDevice)
@@ -125,11 +126,12 @@ CFMutableArrayRef MTDeviceCreateList(void); //returns a CFMutableArrayRef array 
 		}
 		if (![thisDevice isUsable])
 			continue;
+		foundUsableDevice = YES;
 		MTRegisterContactFrameCallback([deviceList objectAtIndex:i], callback); //assign callback for device
 		MTDeviceStart([deviceList objectAtIndex:i], 0); //start sending events
 	}
 	CFRelease((CFMutableArrayRef)deviceList);
-	if ([[self devices] count] < 1) {
+	if (!foundUsableDevice) {
 		NSInteger theResponse = NSRunAlertPanel(@"No supported devices detected",
 				@"We couldn't detect any compatible multitouch device connected to your system.\n"
 				"If a multitouch devie is connected but not detected, please inform us, so that it'll "